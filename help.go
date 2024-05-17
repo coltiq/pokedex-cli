104d042dd542257bf1b0178d58f48c0c6b1a93b3
coltiq <coltonherren97@gmail.com>
2024-05-17 12:05:57 -0500

    clean help cmd

--- help.go
+++ help.go
@@ -6,11 +6,13 @@ import (
 
 
 func commandHelp() error{
-    cliCommands := getCliCommands()
-    fmt.Println("\nWelcome to the Pokedex!\nUsage:\n")
-    for _, command := range cliCommands {
-        fmt.Printf("%v: %v\n", command.name, command.description)
+    fmt.Println()
+    fmt.Println("Welcome to the Pokedex!")
+    fmt.Println("Usage:")
+    fmt.Println()
+    for _, cmd := range getCliCommands() {
+        fmt.Printf("%v: %v\n", cmd.name, cmd.description)
     }
-    fmt.Println("")
+    fmt.Println()
     return nil
 }

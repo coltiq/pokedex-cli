72ee42c68a807b0165c0db9952f952b46df34f2e
coltiq <coltonherren97@gmail.com>
2024-05-28 12:08:50 -0500

    implement catch

--- command_catch.go
+++ command_catch.go
@@ -1,5 +1,28 @@
 package main
 
-func commandCatch(cfg *config, arg ...string) error {
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+    name := &args[0]
+    pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+    if err != nil {
+        return errors.New("Pokemon Does Not Exist")
+    }
+
+    result := rand.Intn(pokemon.BaseExperience)
+
+    fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+    if result > 40 {
+            fmt.Printf("%s escaped\n", pokemon.Name)
+            return nil
+    }
+
+    fmt.Printf("%s was caught!\n", pokemon.Name)
+
+    cfg.pokedex[pokemon.Name] = pokemon
     return nil
 }

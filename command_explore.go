ec9580965ea4a91b5012eb2ffc3648cf8136835f
coltiq <coltonherren97@gmail.com>
2024-05-28 10:35:57 -0500

    explore functionality

--- command_explore.go
+++ command_explore.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func commandExplore(cfg *config, extraCommands []string) error {
-    locationArea := extraCommands[0]
+    locationArea := &extraCommands[0]
     pokemonResp, err := cfg.pokeapiClient.GetPokemon(locationArea)
     if err != nil {
         return err 
     }
 
-    fmt.Println(pokemonResp)
+    pokemonEncounters := pokemonResp.PokemonEncounters
+
+    fmt.Printf("Exploring %s...\n", *locationArea)
+    fmt.Println("Found Pokemon:")
+
+    for _, pokemon := range pokemonEncounters {
+        name := pokemon.Pokemon.Name
+        fmt.Printf(" - %s\n", name)
+    }
 
     return nil
 }

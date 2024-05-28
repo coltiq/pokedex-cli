6e49e8ae574e90fd4b2206599983b949ebb04755
coltiq <coltonherren97@gmail.com>
2024-05-28 12:35:07 -0500

    reformat

--- command_inspect.go
+++ command_inspect.go
@@ -7,26 +7,27 @@ import (
 func commandInspect(cfg *config, args ...string) error {
     name := args[0]
     
-    if pokemon, ok := cfg.pokedex[name]; ok {
-        fmt.Printf("Name: %s\n", pokemon.Name)
-        fmt.Printf("Height: %d\n", pokemon.Height)
-        fmt.Printf("Weight: %d\n", pokemon.Weight)
-
-        fmt.Println("Stats:")
-        fmt.Printf("  -hp: %d\n", pokemon.Stats[0].BaseStat)
-        fmt.Printf("  -attack: %d\n", pokemon.Stats[1].BaseStat)
-        fmt.Printf("  -defense: %d\n", pokemon.Stats[2].BaseStat)
-        fmt.Printf("  -special-attack: %d\n", pokemon.Stats[3].BaseStat)
-        fmt.Printf("  -special-defense: %d\n", pokemon.Stats[4].BaseStat)
-        fmt.Printf("  -speed: %d\n", pokemon.Stats[5].BaseStat)
-
-        fmt.Println("Types: ")
-
-        for _, ele := range pokemon.Types {
-            fmt.Printf("  - %s\n", ele.Type.Name)
-        }
-
-        return nil
+    pokemon, ok := cfg.pokedex[name]
+    if !ok {
+        return fmt.Errorf("%s is not in your Pokedex", name)
     }
-    return fmt.Errorf("%s is not in Pokedex", name)
+
+    fmt.Printf("Name: %s\n", pokemon.Name)
+    fmt.Printf("Height: %d\n", pokemon.Height)
+    fmt.Printf("Weight: %d\n", pokemon.Weight)
+
+    fmt.Println("Stats:")
+    fmt.Printf("  -hp: %d\n", pokemon.Stats[0].BaseStat)
+    fmt.Printf("  -attack: %d\n", pokemon.Stats[1].BaseStat)
+    fmt.Printf("  -defense: %d\n", pokemon.Stats[2].BaseStat)
+    fmt.Printf("  -special-attack: %d\n", pokemon.Stats[3].BaseStat)
+    fmt.Printf("  -special-defense: %d\n", pokemon.Stats[4].BaseStat)
+    fmt.Printf("  -speed: %d\n", pokemon.Stats[5].BaseStat)
+
+    fmt.Println("Types: ")
+    for _, ele := range pokemon.Types {
+        fmt.Printf("  - %s\n", ele.Type.Name)
+    }
+
+    return nil
 }
